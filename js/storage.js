8b7d7cead1b177ac1d92b23965d918b4c03925c3
DESKTOP-9BAF72R\Hoon <ssang276@sk.com>
2021-01-12 18:33:49 +0900

    Bind Checkboxes with String Array

--- js/storage.js
+++ js/storage.js
@@ -14,16 +14,16 @@
   }
 
   save([
-    { name: 'Frozen Yogurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, iron: '1%', glutenfree: true },
-    { name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, iron: '1%', glutenfree: true },
-    { name: 'Eclair', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, iron: '7%', glutenfree: true },
-    { name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, iron: '8%', glutenfree: true },
-    { name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%', glutenfree: true },
-    { name: 'Jelly bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, iron: '0%', glutenfree: true },
-    { name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0, iron: '2%', glutenfree: true },
-    { name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, iron: '45%', glutenfree: true },
-    { name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, iron: '22%', glutenfree: true },
-    { name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7, iron: '6%', glutenfree: true }
+    { name: 'Frozen Yogurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, iron: '1%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, iron: '1%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Eclair', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, iron: '7%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, iron: '8%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, iron: '16%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Jelly bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, iron: '0%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0, iron: '2%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, iron: '45%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, iron: '22%', tags: ['glutenfree', 'lowfat', 'saltfree'] },
+    { name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7, iron: '6%', tags: ['glutenfree', 'lowfat', 'saltfree'] }
   ]);
 
   exports.storage = {
